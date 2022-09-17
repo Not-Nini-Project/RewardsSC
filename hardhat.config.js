6191fd16f5b94f4734ac4069aa3174e865b4dd62
TheMrMatt <matt.rojas70@gmail.com>
2022-09-17 14:34:43 -0300

    adawd

---
+++ hardhat.config.js
@@ -0,0 +1,59 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+task("deploy", "Deploy the smart contracts", async(taskArgs, hre) => {
+
+  const Artwork = await hre.ethers.getContractFactory("Artwork");
+  const artwork = await Artwork.deploy("Artwork Contract", "ART");
+
+  await artwork.deployed();
+
+  await hre.run("verify:verify", {
+    address: artwork.address,
+    constructorArguments: [
+      "Artwork Contract",
+      "ART"
+    ]
+  })
+
+})
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
